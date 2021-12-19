27d53689f71a15f0b066d5d66424fc6008d31f3d
andrey12v <andrey12v@gmail.com>
2021-12-19 15:56:33 -0600

    Create Expenses Component and render Expense Item in it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
 
@@ -27,11 +27,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem 
-        date={expenses[0].date}
-        title={expenses[0].title}
-        amount={expenses[0].amount}>
-      </ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
