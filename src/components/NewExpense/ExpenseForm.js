8a5bbae27f82437ffce159d5b65db0160a53dae2
andrey12v <andrey12v@gmail.com>
2021-12-19 22:18:42 -0600

    Implement OnSubmit Event Handler in the form and Get Values in the Object

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,21 +4,19 @@ import './ExpenseForm.css';
 
 const ExpenseForm = () => {
     
-  /*const [eneteredTitle, setEneteredTitle] = useState('');   
-  const [eneteredAmount, setEneteredAmount] = useState('');  
-  const [eneteredDate, setEneteredDate] = useState(''); */
+  const [enteredTitle, setEnteredTitle] = useState('');   
+  const [enteredAmount, setEnteredAmount] = useState('');  
+  const [enteredDate, setEnteredDate] = useState(''); 
 
-    const [userInput, setUserInput] = useState({
+    /*const [userInput, setUserInput] = useState({
         enteredTitle: '',
         enteredAmount: '',
         enteredDate: ''
-    });
+    });*/
 
-  //when we type something in the input field we get value as an object from the input field
-  //in order to get the values from this object we access it with this code: event.target.value 
   const titleChangeHandler = (event) => 
   {
-    //setEneteredTitle(event.target.value);
+    setEnteredTitle(event.target.value);
     
     //here we use spread operator to pass values to all properties of the object
     //then we override title with the value form the input field. 
@@ -32,34 +30,50 @@ const ExpenseForm = () => {
     //The new function gets values from teh previous statess and assigns them with the help of spread opertator  
     //this function will gureantee that it will always be the latest snapshot value of the state. 
     //if you depend on the previous state then use this code. 
-    setUserInput((prevState) => {
+    /*setUserInput((prevState) => {
         return {...prevState, enteredTitle: event.target.value };
-    });
+    });*/
 
 
 };
 
   const amountChangeHandler = (event) => 
   {
-    //setEneteredAmount(event.target.value);
-    setUserInput({
+    setEnteredAmount(event.target.value);
+    
+    /*setUserInput({
         ...userInput,
         enteredAmount: event.target.value 
-    })
+    })*/
+
   };
 
   const dateChangeHandler = (event) => 
   {
-    //setEneteredDate(event.target.value);
-    setUserInput({
+    setEnteredDate(event.target.value);
+    
+    /*setUserInput({
         ...userInput,
         enteredDate: event.target.value 
-    })
+    })*/
+
   };
 
+  const submitHandler = (event) => {
+      event.preventDefault();
+      
+      //here we create object and reference to the entered Ttitle from the state object
+      const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+      };
+
+      console.log(expenseData);
+  };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
@@ -76,7 +90,7 @@ const ExpenseForm = () => {
         </div>
         
         <div className='new-expense__actions'>
-            <button type="submit">Add Expense</button>
+            <button type="submit" >Add Expense</button>
         </div>
 
     </form>
