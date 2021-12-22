64255a4a1564788baa8f6f347c6faf4a789920ed
andrey12v <andrey12v@gmail.com>
2021-12-21 23:41:42 -0600

    Implement Year Filter - pass selected year from ExpensesFilter to Expenses
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/practice/1290022/instructor-solution#overview

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,28 @@
 //import ExpenseDate from './ExpenseDate';
 import './Expenses.css';
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
 
+  const filterChangeHandler = (selectedYear) => {
+    console.log('Expenses.js');
+    console.log(selectedYear);
+  }  
+
   return (
-    <Card className="expenses">
+    <div>
+      
+      <Card className="expenses">
+        <ExpensesFilter onChangeFilter={filterChangeHandler} />
         <ExpenseItem 
             date={props.items[0].date}
             title={props.items[0].title}
             amount={props.items[0].amount}>
-      </ExpenseItem>
-    </Card>
+        </ExpenseItem>
+      </Card>
+    </div>
   );
 }
 
